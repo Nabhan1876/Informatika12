bfb626deb70dbb21669d8da0750f8e4edfbde8f3
Nabhan1876 <144643367+Nabhan1876@users.noreply.github.com>
2024-08-18 22:42:20 +0700

    Add files via upload

---
+++ Contoh Kodingan Sederhana_Nabhan Ahmad Mubarok.py
@@ -0,0 +1,11 @@
+# Program untuk menjumlahkan dua angka
+
+# Meminta input dari pengguna
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+
+# Menghitung penjumlahan
+jumlah = angka1 + angka2
+
+# Menampilkan hasil
+print("Hasil penjumlahan dari", angka1, "dan", angka2, "adalah", jumlah)
